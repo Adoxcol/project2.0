dc98bcf7ab88985edbc9b6ac7d45173df5fe5435
adoxcol <onelittle1221@gmail.com>
2025-03-26 01:02:24 +0600

    fixed the tailwind vite configuration error and updated home page to navigate through all pages

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Go to Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Go to Register</Link>
+          </li>
+          <li>
+            <Link to="/profile">Go to Profile</Link>
+          </li>
+          <li>
+            <Link to="/posts">Go to Posts</Link>
+          </li>
+          <li>
+            <Link to="/categories">Go to Categories</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default HomePage;
